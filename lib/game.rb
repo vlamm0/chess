a106481b7752b44d8b04f3c8b50cbb3bd1bb466b
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2025-01-05 10:02:01 -0500

    Add comments

--- lib/game.rb
+++ lib/game.rb
@@ -8,16 +8,19 @@ require_relative 'data'
 class Chess
   attr_accessor :data
 
+  # uses the required data file for state, start or saved (to be implemented!)
   def initialize
-    @data = MyData.board # Array.new(8) { Array.new(8, '') }
+    @data = MyData.board
   end
 
-  def draw_square(row, col, piece = data[row][col])
+  # draws a chess tile with the piece data
+  def draw_tile(row, col, piece = data[row][col])
     light_square = piece == '' ? "\e[48;5;255m   \e[0m" : "\e[48;5;255m \e[38;5;0m#{piece.sym} \e[0m"
     dark_square = piece == '' ? "\e[48;5;244m   \e[0m" : "\e[48;5;244m \e[38;5;0m#{piece.sym} \e[0m"
     (row + col).even? ? light_square : dark_square
   end
 
+  # draws the entire board, including the row/col headers
   def chessboard
     # Loop to create 8 rows of chessboard
     alph = %w[A B C D E F G H]
@@ -25,7 +28,7 @@ class Chess
     puts
     8.times do |row|
       print "#{alph[row]} "
-      8.times { |col| print draw_square(row, col) }
+      8.times { |col| print draw_tile(row, col) }
       puts
     end
   end
